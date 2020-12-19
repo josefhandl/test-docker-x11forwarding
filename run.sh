3d76a57ab3bcf6cf74a752015679f20099e86258
Josef Handl <handljosef@gmail.com>
2020-12-19 10:47:34 +0100

    added DRI_PRIME env | changed .Xauthority mount from 'rw' to 'ro'

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker run --rm -it --device=/dev/dri:/dev/dri --net=host --env="DISPLAY" --volume="$HOME/.Xauthority:/root/.Xauthority:rw" --name=test-docker-x11forwarding  test-docker-x11forwarding
+docker run --rm -it --device=/dev/dri:/dev/dri --net=host --env="DISPLAY" --env="DRI_PRIME=0" --volume="$HOME/.Xauthority:/root/.Xauthority:ro" --name=test-docker-x11forwarding  test-docker-x11forwarding
 
 #--device=/dev/dri:/dev/dri
